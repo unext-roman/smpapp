88c61230e49693c5c5bcd8314f597078690e4cd3
unext-roman <r-ahmed@unext.jp>
2016-08-18 18:44:14 +0900

    Create constItems.rb

---
+++ constItems.rb
@@ -0,0 +1,41 @@
+#############################################################################
+# 課題 : スマホアプリ向け回帰テストの自動化
+# モジュール : 定数バリエブル定義
+# 開発者 : Roman Ahmed
+# コピーライト : U-NEXT Co. Ltd.
+# バージョン : v1.0
+#############################################################################
+
+	$resultOK = "OK"
+	$resultNG = "NG"
+	$result = ""
+	$passCount = 0
+	$totalTest = 0
+	$finishedTest = 0
+	$errMsg = ""
+	$comment = ""
+	$count = 0
+	$exeTime = ""
+	$captureURL = ""
+	$tcs = 0
+
+	#Error Message List
+	$errMsgLogin = ""
+	$errMsgTanwa = ""
+	$errMsgConti = ""
+	$errMsgLogot = ""
+	$errMsgBuypv = ""
+	$errMsgHisto = ""
+	$errMsgBougt = ""
+
+
+	#class names
+
+	#testLogin			:Login
+	#funcSinglePlay		:SinglePlay 	:$obj_login
+	#funcContinuePlay	:ContinuePlay 	:$obj_contp
+	#funcBuyPPV 		:BuyPPV 		:$obj_buypv
+	#funcHistoryPlay	:HistoryPlay 	:$obj_histp
+	#funcPurchasedPlay 	:PurchasePlay 	:$obj_prcsp
+	#funcMylistPlay 	:MyList 		:$obj_mylst
+	#funcLogout 		:Logout 		:$obj_logot
