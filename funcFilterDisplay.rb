c4eee821ef5ffcfc96c1875468ae0d832c3e0a8a
unext-roman <r-ahmed@unext.jp>
2016-12-13 18:46:55 +0900

    Add files via upload

--- funcFilterDisplay.rb
+++ funcFilterDisplay.rb
@@ -261,8 +261,8 @@ class FilterSearch
 	def ifilterOperation(client)
 
 		begin
-			#client.click("NATIVE", "xpath=//*[@class='UIImageView' and @height>0 and ./parent::*[@accessibilityLabel='button search']]", 0, 1)
-			client.click("NATIVE", "xpath=//*[@class='UIImageView' and @height>0 and ./following-sibling::*[@class='UIButtonLabel'] and ./parent::*[@class='UIButton' and ./parent::*[@class='UNextMobile_Protected.UNChromecastButtonContainer']]]", 0, 1)
+			client.click("NATIVE", "xpath=//*[@class='UIImageView' and @height>0 and ./parent::*[@accessibilityLabel='button search']]", 0, 1)
+			#client.click("NATIVE", "xpath=//*[@class='UIImageView' and @height>0 and ./following-sibling::*[@class='UIButtonLabel'] and ./parent::*[@class='UIButton' and ./parent::*[@class='UNextMobile_Protected.UNChromecastButtonContainer']]]", 0, 1)
 			client.sleep(2000)
 			client.click("NATIVE", "xpath=//*[@text='洋画一覧']", 0, 1)
 			client.sleep(2000)
